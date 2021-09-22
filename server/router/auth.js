7aa8e7bd13031d1aa31d602b5c7fe4389a6289ff
Adol <joyaadol@gmail.com>
2021-09-23 02:00:42 +0600

    pass bcrypt

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+const bcrypt = require("bcryptjs");
+
 require("../db/dbconn");
 const User = require("../models/userSchema");
 
@@ -10,6 +12,9 @@ router.post("/register", async (req, res) => {
   if (!uName || !uEmail || !uPassword || !uConfirmPassword) {
     return res.status(422).json({ error: "Fill all fields!" });
   }
+  if (uPassword != uConfirmPassword) {
+    return res.status(422).json({ error: "Password didn't match!" });
+  }
 
   try {
     const userExist = await User.findOne({ uEmail: uEmail });
@@ -17,7 +22,9 @@ router.post("/register", async (req, res) => {
       return res.status(422).json({ error: "Email Already Exist!" });
     }
 
-    const newUser = new User({ uName, uEmail, uPassword, uGender });
+    const salt = await bcrypt.genSalt((saltRounds = 10));
+    const hasPass = await bcrypt.hash(uPassword, salt);
+    const newUser = new User({ uName, uEmail, uPassword: hasPass, uGender });
 
     await newUser.save();
     res.status(201).json("User registration successful!");
@@ -36,6 +43,11 @@ router.post("/login", async (req, res) => {
   try {
     const userExist = await User.findOne({ uEmail: uEmail });
     if (!userExist) {
+      return res.status(422).json({ error: "Wrong email of password!" });
+    }
+
+    const isMatch = await bcrypt.compare(uPassword, userExist.uPassword);
+    if (!isMatch) {
       return res.status(422).json({ error: "Wrong email of password!" });
     }
     res.status(201).json("Login in successfull!");
