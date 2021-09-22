3f74db1e1896ad96e0b60b897cce919314270392
Adol <joyaadol@gmail.com>
2021-09-23 01:19:06 +0600

    login router create

--- server/router/auth.js
+++ server/router/auth.js
@@ -4,31 +4,44 @@ const router = express.Router();
 require("../db/dbconn");
 const User = require("../models/userSchema");
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   const { uName, uEmail, uPassword, uConfirmPassword, uGender } = req.body;
 
   if (!uName || !uEmail || !uPassword || !uConfirmPassword) {
     return res.status(422).json({ error: "Fill all fields!" });
   }
 
-  User.findOne({ uEmail: uEmail })
-    .then((userExist) => {
-      if (userExist) {
-        return res.status(422).json({ error: "Email Already Exist!" });
-      }
-      const newUser = new User({ uName, uEmail, uPassword, uGender });
-      newUser
-        .save()
-        .then(() => {
-          res.status(201).json("User registration successful!");
-        })
-        .catch((error) => {
-          res.status(500).json({ error: "registration failed!" });
-        });
-    })
-    .catch((error) => {
-      console.log("reg error!");
-    });
+  try {
+    const userExist = await User.findOne({ uEmail: uEmail });
+    if (userExist) {
+      return res.status(422).json({ error: "Email Already Exist!" });
+    }
+
+    const newUser = new User({ uName, uEmail, uPassword, uGender });
+
+    await newUser.save();
+    res.status(201).json("User registration successful!");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { uEmail, uPassword } = req.body;
+
+  if (!uEmail || !uPassword) {
+    return res.status(422).json({ error: "Fill all fields!" });
+  }
+
+  try {
+    const userExist = await User.findOne({ uEmail: uEmail });
+    if (!userExist) {
+      return res.status(422).json({ error: "Wrong email of password!" });
+    }
+    res.status(201).json("Login in successfull!");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
