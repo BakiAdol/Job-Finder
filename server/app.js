c5d87c717967581ac1c4da8f2618a34b4f2dcb25
Adol <joyaadol@gmail.com>
2021-09-22 17:59:00 +0600

    connect with local database

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
+mongoose
+  .connect("mongodb://localhost:27017/job-finder", {
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log("Database connected!");
+  })
+  .catch((err) => {
+    console.log("Databse connection failed!");
+  });
+
 app.get("/", (req, res) => {
-  res.send("Hello from server");
+  res.send("Hello from node server");
 });
 
 app.listen(4000, () => {
-  console.log("Server is running on port 400");
+  console.log("Server is running on port 4000");
 });
